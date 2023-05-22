e6d18302325b6f65e5252986610bd1e80aab77b1
Jaron <jaronchretien@yahoo.com>
2023-05-22 12:49:06 -0500

    feat(login): Login page unfisihed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,30 @@
 
 
 const Login = () => {
+    
     return (
-        <h2>Login page</h2>
+        <div className="loginPage">
+            <article >
+                <div className="loginContainer">
+                    <div className="loginForm">
+                        <h2>Login!</h2>
+                        <div className="loginInput">
+                            <label>Username</label>
+                            <input></input>
+                        </div>
+                        <div className="loginInput">
+                            <label>Password</label>
+                            <input></input>
+                        </div>
+                        <button className="loginButton">Login</button>
+                    </div>
+                
+                    <div className="logoContainer">
+                        
+                    </div>
+                </div>
+            </article> 
+        </div>
     )
 }
 
