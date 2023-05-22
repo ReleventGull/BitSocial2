d59950515dfa2ade32d2239b221aa26a3181e627
Jaron <jaronchretien@yahoo.com>
2023-05-22 10:25:49 -0500

    feat(routes) Sets up routes for login page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 const root = ReactDOM.createRoot(document.getElementById('app'))
 
 root.render(
     <React.StrictMode>
+        <BrowserRouter>
          <App />
+        </BrowserRouter>
     </React.StrictMode>
 )
