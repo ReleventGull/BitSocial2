3eaf208be6ea48b08b32775bd47b078da44eb604
Jaron <jaronchretien@yahoo.com>
2023-05-24 09:06:47 -0500

    feat(message) Creates chat and message table

--- db/init.js
+++ db/init.js
@@ -8,6 +8,18 @@ const buildTables = async() => {
             username VARCHAR(15) NOT NULL,
             password VARCHAR(20) NOT NULL
         );
+        CREATE TABLE chat (
+            id SERIAL PRIMARY KEY,
+            user_id_1 INTEGER REFERENCES users(id),
+            user_id_2 INTEGER REFERENCES users(id)
+        );
+        CREATE TABLE message (
+            id SERIAL PRIMARY KEY,
+            chat_id INTEGER REFERENCES chat(id),
+            user_id INTEGER REFERENCES users(id),
+            message VARCHAR(255) NOT NULL,
+            date VARCHAR(255) NOT NULL
+        );
         `)
         console.log("Finished building tables")
     }catch(error) {
@@ -19,7 +31,9 @@ const buildTables = async() => {
 const dropTables = async() => {
     try {
         await client.query(`
-        DROP TABLE IF EXISTS users
+        DROP TABLE IF EXISTS message;
+        DROP TABLE IF EXISTS chat;
+        DROP TABLE IF EXISTS users;
         `)
         console.log('Finished dropping tables')
     }catch(error) {
