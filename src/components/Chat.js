d0b9a8d726a1aae88b14a08db4eeccefcda26a93
Jaron <jaronchretien@yahoo.com>
2023-05-25 16:52:11 -0500

    feat(chatSearch): Adds saerch input for seearching as well as tab for current users

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,29 @@
+import { useState } from "react"
 
-
-
+const activeStyle = {
+    backgroundColor: 'var(--third)',
+    borderRadius: '12px',
+    color: 'var(--primary)',
+    transition: '.2s'
+}
 
 const Chat = () => {
+const [active, setActive] = useState('search')
     return (
         <div className="outlet Chat">
-
+            <div className="chatSelectionBox">
+                <div className="topChatSelection">
+                    <div onClick={() => setActive('current')} className="chatSelectionDiv" style={(active == 'current' ? activeStyle : null)}>
+                        <h2 className="chatSelectionButtons">Current</h2>
+                    </div>
+                    <div  onClick={() => setActive('search')} className="chatSelectionDiv" style={(active == 'search' ? activeStyle : null)}>
+                        <h2 className="chatSelectionButtons">Search</h2>
+                    </div>
+                </div>
+                {active != 'search' ? null : 
+                    <input className="searchInput" placeholder="Search..."></input>
+                }
+            </div>
         </div>
     )
 }
