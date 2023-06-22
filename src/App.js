fd0df49c943fda4aadd596b359347dddefabc1f3
Jaron <jaronchretien@yahoo.com>
2023-06-22 17:41:50 -0500

    feat(friend_request): Adds th socket for sending and revking friend request

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import {Route, Routes, useNavigate} from 'react-router-dom'
 import {Login, NavBar, Home, Chat, Profile, Settings, Friend} from './components/index'
 import { All, Pending, FriendRequest, SearchFriends, } from './components/FriendsComponents'
 import {io} from 'socket.io-client'
-let socket;
+
 const App = () => {
     const [token, setToken] = useState(window.localStorage.getItem('token') || '')
     const [sentMessage, setSentMessage] = useState('')
     const [notifClass, setNotifClass] = useState('')
     const [counter, setCounter] = useState(0)
+    const [socket, setSocket] = useState('')
     const navigate = useNavigate()
     
 
@@ -17,11 +18,14 @@ const App = () => {
         if(!token){
             navigate('login')
         }else {
-                 socket = io.connect('http://localhost:3000', {
+                 setSocket(
+                    io.connect('http://localhost:3000', {
                     auth: {
                     token: token
                     }
                 })
+
+                 ) 
         }
     }, [token])
 
@@ -48,24 +52,30 @@ const App = () => {
     }, [counter])
 
 
-    
     return (
         <>
+        {!socket ? null :
         <Routes>
-            <Route path='login' element={<Login token={token} setToken={setToken}/>}/>
-            <Route path='/' element={<NavBar token={token} sentMessage={sentMessage} notifClass={notifClass} setToken={setToken}/>}>
-                <Route path='home' element={<Home />}/>
-                <Route path='chat' element={<Chat token={token}/>}/>
-                    <Route path="friend" element={<Friend />}>
-                        <Route path='all' element={<All token={token} />}/>
-                        <Route path='pending' element={<Pending socket={socket} setCounter={setCounter} notifClass={notifClass} setSentMessage={setSentMessage} setNotifClass={setNotifClass} token={token}/>}/>
-                        <Route path='request' element={<FriendRequest socket={socket} setCounter={setCounter} notifClass={notifClass} setSentMessage={setSentMessage} setNotifClass={setNotifClass} token={token}/>}/>
-                        <Route path='search' element={<SearchFriends setCounter={setCounter} notifClass={notifClass} setSentMessage={setSentMessage} setNotifClass={setNotifClass} token={token}/>}/>
-                    </Route>
-                <Route path='profile' element={<Profile />}/>
-                <Route path='settings' element={<Settings setToken={setToken}/>}/>
-            </Route>
-        </Routes>
+        <Route path='login' element={<Login token={token} setToken={setToken}/>}/>
+        <Route path='/' element={<NavBar socket={socket} token={token} sentMessage={sentMessage} notifClass={notifClass} setToken={setToken}/>}>
+            <Route path='home' element={<Home />}/>
+            <Route path='chat' element={<Chat token={token}/>}/>
+                <Route path="friend" element={<Friend />}>
+                    <Route path='all' element={<All token={token} />}/>
+                    <Route path='pending' element={<Pending socket={socket} setCounter={setCounter} notifClass={notifClass} setSentMessage={setSentMessage} setNotifClass={setNotifClass} token={token}/>}/>
+                    <Route path='request' element={<FriendRequest socket={socket} setCounter={setCounter} notifClass={notifClass} setSentMessage={setSentMessage} setNotifClass={setNotifClass} token={token}/>}/>
+                    <Route path='search' element={<SearchFriends socket={socket} setCounter={setCounter} notifClass={notifClass} setSentMessage={setSentMessage} setNotifClass={setNotifClass} token={token}/>}/>
+                </Route>
+            <Route path='profile' element={<Profile />}/>
+            <Route path='settings' element={<Settings setToken={setToken}/>}/>
+        </Route>
+    </Routes>
+        
+        
+        
+        
+        }
+        
         </>
     )
 }
