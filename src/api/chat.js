6723cab75ef087e6adc097a1145fef163a112361
Jaron <jaronchretien@yahoo.com>
2023-07-31 16:01:32 -0500

    feat(chat): Renders chat when create

--- src/api/chat.js
+++ src/api/chat.js
@@ -36,3 +36,18 @@ export const getUserChats = async(token) => {
         throw error
     }
 }
+
+export const getChatById = async({token, chatId}) => {
+    try {
+        const response = await fetch(`${BASE_URL}/chat/${chatId}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-type': 'application/json'
+            }
+        }).then(result => result.json())
+        return response 
+    }catch(error) {
+        console.error("There was an error creating the chat", error)
+        throw error
+    }
+}
