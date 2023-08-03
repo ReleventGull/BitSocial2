b4ced7d984dac1050e12a24be34c0fb59479d336
Jaron <jaronchretien@yahoo.com>
2023-08-03 12:26:48 -0500

    feat(styling): Gets simple styling done for the chat page

---
+++ src/components/MessageItem.js
@@ -0,0 +1,35 @@
+
+
+
+
+const MessageItem = ({item, i, arr}) => {
+
+    return (
+        <div style={{marginTop : (i - 1 < 0  || arr[i].user_id !== arr[i-1].user_id ? '1rem': '0')}} className="messageBox">
+            {
+            i - 1 < 0  || arr[i].user_id !== arr[i-1].user_id ? 
+                <>
+                <div className="circleMessageBox">
+                    <div className="cirlce">
+                    <img src='/images/Person.png'/>
+                    </div>
+                </div>
+                <div className="usernameMessage">
+                    <h3>{item.username}</h3>
+                    <p className="message">{item.message}</p>
+                </div>
+                </>
+
+            :
+            <>
+            <div className="singleMessage">
+            <p className="message">{item.message}</p>
+            </div>
+            </>
+            }
+
+        </div>
+    )
+}
+
+export default MessageItem
