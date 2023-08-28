a5095eb9c74be0d91b7884a9cb11262d52db4598
Jaron <jaronchretien@yahoo.com>
2023-08-28 17:00:42 -0500

    feat(outlet): Sets up outlet for the settigns page

---
+++ src/components/Settings.js
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+
+const Settings = () => {
+    return (
+        <div className="settingsPage">
+            <div>This is where shit goes</div>
+
+
+
+        <Outlet />
+        
+
+
+
+
+
+        </div>
+    )
+}
+
+export default Settings
